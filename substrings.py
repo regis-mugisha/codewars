7a3a52cd590a50b9f6d7e51d2121093004de329a
regis-mugisha <mugisharegis72@gmail.com>
2025-10-20 15:36:15 +0200

    optimize solution

--- substrings.py
+++ substrings.py
@@ -7,5 +7,6 @@ def in_array(array1, array2):
             if substring in main_string:
                 if substring not in substring_array:
                     substring_array.append(substring)
+                    break
 
     return sorted(substring_array)
