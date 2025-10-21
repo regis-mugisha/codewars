4ce30aad68ffd4cc6d999c2f65f9e22afc825963
regis-mugisha <mugisharegis72@gmail.com>
2025-10-21 16:24:48 +0200

    add [String incrementer] kata solution

---
+++ string-incrementer.py
@@ -0,0 +1,6 @@
+def increment_string(s):
+    head = s.rstrip("0123456789")
+    tail = s[len(head) :]
+    if tail == "":
+        return s + "1"
+    return head + str(int(tail) + 1).zfill(len(tail))
